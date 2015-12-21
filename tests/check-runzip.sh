f2a47511bba3136db368a526942173c72a0b4d14
Lev Walkin <vlm@lionet.info>
2015-12-21 00:21:06 -0800

    remove zipdetails; portability

--- tests/check-runzip.sh
+++ tests/check-runzip.sh
@@ -10,10 +10,10 @@ cp -r originals processed
 
 ${RUNZIP} processed/*.zip
 
-zipdetails processed/windows-archive.zip
+# zipdetails processed/windows-archive.zip
 unzip -d processed/windows processed/windows-archive.zip
 
-zipdetails processed/mac-archive.zip
+# zipdetails processed/mac-archive.zip
 unzip -d processed/mac processed/mac-archive.zip
 
 ls -alR processed
